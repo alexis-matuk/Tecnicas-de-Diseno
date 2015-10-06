bb28ab34b254d8d03365661cb3d7f368a6731432
Alexis_Matuk <alexis.matuk@gmail.com>
2015-10-06 13:27:56 -0500

    Reestructuración de párrafos

--- Parrafos.cpp
+++ Parrafos.cpp
@@ -6,6 +6,10 @@
 //  Copyright (c) 2015 alexis matuk. All rights reserved.
 //
 
+// Checar que el coste esté bien calculado
+// Probar con más casos
+// Imprimir la palabra al final del for, no toda la línea
+
 #include <stdio.h>
 #include <math.h>
 #include <iostream>
@@ -19,7 +23,7 @@ float bPrime(int maxLength, int totalWordsLength, int i, int j)
 
 int main(int argc, const char * argv[])
 {
-    std::vector<std::string> words = {"The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"};
+    std::vector<std::string> words = {"The", " ", "quick", " ", "brown", " ", "fox", " ", "jumps", " ", "over", " ", "the", " ", "lazy", " ", "dog"};
     std::vector<size_t>wordLengths;
     std::string space = " ";
     for (auto i : words)
@@ -27,69 +31,88 @@ int main(int argc, const char * argv[])
     std::vector<std::string> currentLine;
     std::vector<int> spacePositions;
     int maxLength = 20;
-    float idealSize = 1.0;
+    float idealSize = 1;
     int numberOfSpaces = 0;
     int totalWordsLength = 0;
     int startingWord = 0;
     int currentWord = -1;
+    int currLinePos = -1;
     for(int i=0; i<words.size(); i++)
     {
         currentLine.push_back(words[i]);//agregar la palabra y ver que pasaría después
-        currentLine.push_back(space);
-        numberOfSpaces++;
-        totalWordsLength += wordLengths[i];
-        currentWord++;
-<<<<<<< HEAD
-        spacePositions.push_back(currentWord+1);
-        if(totalWordsLength+numberOfSpaces*idealSize > maxLength)//Si la línea se pasa del máximo
+        currLinePos++;
+        if(words[i] == " ")
         {
-            numberOfSpaces--;
-            currentLine.pop_back();
+            numberOfSpaces++;
+            spacePositions.push_back(currentWord);
+        }
+        else
+        {
+            totalWordsLength += wordLengths[i];
+            currentWord++;
+        }
+        if(totalWordsLength+(numberOfSpaces*idealSize) > maxLength)//Si la línea se pasa del máximo
+        {
+            //numberOfSpaces--;
+            //currentLine.pop_back();
             //Si la palabra ya no cabe hay dos opciones.
             //Primero se quita la palabra para manejar la línea
-            if(numberOfSpaces*idealSize >= words[i].length())//Si cabe quitando todos los espacios
+            if((numberOfSpaces*idealSize) >= words[i].length())//Si cabe quitando todos los espacios
             {
                 //Si cabe, se tiene la opción de reducir la línea o pasar la palabra a la siguiente
                 float costReduce = 0;
                 float costExtend = 0;
                 /*Reducir*/
                 costReduce = fabs(bPrime(maxLength, totalWordsLength, startingWord, currentWord) - (float)idealSize)*(currentWord-startingWord);
-                currentLine.pop_back();
-                totalWordsLength -= wordLengths[i];
-                currentWord--;
+                if(words[i] == " ")
+                {
+                    numberOfSpaces--;
+                    currentLine.pop_back();
+                    
+                }
+                else
+                {
+                    currentLine.pop_back();
+                    totalWordsLength -= wordLengths[i];
+                    currentWord--;
+                }
                 /*Extender*/
                 costExtend = fabs(bPrime(maxLength, totalWordsLength, startingWord, currentWord) - (float)idealSize)*(currentWord-startingWord);
-                if(costReduce < costExtend)
+                if(costReduce < costExtend)//CHECAR ESTA FUNCIÓN COMPLETA
                 {
                     //Reducir cuesta menos
                     int neededSize = words[i].length();
                     int aux=0;
-                    while(neededSize != 0)
+                    while(neededSize > 0)
                     {
                         currentLine.erase(currentLine.begin() + spacePositions[aux]);
                         //currentLine[spacePositions[aux]].pop_back();
                         aux++;
-                        neededSize--;
+                        neededSize-=idealSize;
                     }
                     currentLine.push_back(words[i]);
-                    totalWordsLength+=wordLengths[i];
+                    if(words[i] == " ")
+                        numberOfSpaces++;
+                    else
+                        totalWordsLength+=wordLengths[i];
                     //Saltar a la siguiente línea
-                    for(auto i : currentLine)
-                        std::cout << i << std::flush;
                     currentLine.clear();
                     startingWord = currentWord;
                     numberOfSpaces = 0;
                     totalWordsLength = 0;
                     std::cout << std::endl;
                 }
-                else
+                else//CHECAR ESTE PRINT
                 {
                     //Extender cuesta menos
                     //Saltar a la siguiente línea
-                    for(auto i : currentLine)
-                        std::cout << i << std::flush;
-                    currentLine.clear();
                     currentLine.push_back(words[i]);
+                    if(words[i] == " ")
+                        numberOfSpaces++;
+                    else
+                        totalWordsLength+=wordLengths[i];
+                    //Saltar a la siguiente línea
+                    currentLine.clear();
                     startingWord = currentWord;
                     numberOfSpaces = 0;
                     totalWordsLength = 0;
@@ -97,18 +120,18 @@ int main(int argc, const char * argv[])
                 }
             }
             else//Si no cabe, se debe pasar a la siguiente línea
+                //CHECAR ESTA CONDICIÓN
             {
-                totalWordsLength-=wordLengths[i];
-                currentLine.pop_back();
-                for(auto i : currentLine)
-                    std::cout << i << std::flush;
-                currentLine.clear();
                 currentLine.push_back(words[i]);
-                currentLine.push_back(space);
+                if(words[i] == " ")
+                    numberOfSpaces++;
+                else
+                    totalWordsLength+=wordLengths[i];
+                //Saltar a la siguiente línea
+                currentLine.clear();
                 startingWord = currentWord;
                 numberOfSpaces = 0;
-                totalWordsLength = wordLengths[i];
-                numberOfSpaces++;
+                totalWordsLength = 0;
                 std::cout << std::endl;
             }
         }
@@ -116,8 +139,7 @@ int main(int argc, const char * argv[])
         {
             
         }
+        std::cout << currentLine[currLinePos] << std::flush;
     }
-    for(auto i : currentLine)
-        std::cout << i << std::flush;
     return 0;
 }
